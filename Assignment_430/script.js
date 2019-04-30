6ac2bb9eb4989414ea7dabef97424ed2ab8af397
Jenny Zheng <tz1168@nyu.edu>
2019-04-29 23:06:53 -0400

    homework8

---
+++ Assignment_430/script.js
@@ -0,0 +1,6 @@
+function decoration(){
+    var rabbitear=document.createElement("IMG");
+    rabbitear.setAttribute("src","http://images.669pic.com/element_min_new_pic/50/33/78/31/e8d76febffc7dd9f20d9b2005e01f8b5.png!/fw/260/quality/90/unsharp/true/canvas/260x260/compress/true/cvscolor/ffffffff");
+    rabbitear.setAttribute("id","deco");
+    document.body.appendChild(rabbitear);
+}
